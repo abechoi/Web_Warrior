cc4ecd24eb37d98d84e0af036cad3baaa826349b
Abraham Choi <abraham.choi@icloud.com>
2021-04-22 18:32:15 -0700

    Pages & Routes

---
+++ src/pages/about.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur autem
+        aperiam vero! Atque, iusto mollitia unde obcaecati voluptatem illum in
+        iste sit deserunt quod nam explicabo voluptatibus eligendi fugit illo.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde nam velit
+        temporibus error aut itaque ad aliquam nostrum minus cupiditate tempora
+        cum, reiciendis dolor libero corporis saepe recusandae, molestiae odit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente
+        corrupti consequatur fugiat ipsa non ad doloremque repudiandae culpa!
+        Earum placeat quibusdam dicta mollitia aliquam quam quis praesentium
+        soluta atque reprehenderit?
+      </p>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <div>
+        <h2>Design</h2>
+        <h3>Develop & Deploy</h3>
+        <p>UX Designer & web developer based in California</p>
+      </div>
+    </section>
+  )
 }
