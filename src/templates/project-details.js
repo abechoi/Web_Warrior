806d4d59c7c187e337b2ecece5ab71cf953d1324
Abraham Choi <abraham.choi@icloud.com>
2021-04-23 22:36:57 -0700

    Generating Pages

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,17 +1,19 @@
 import React from "react"
 import Layout from "../components/Layout"
 import Img from "gatsby-image"
-import styles from "../styles/project-details.module.css"
+import * as styles from "../styles/project-details.module.css"
 
-export default function ProjectDetails() {
+const ProjectDetails = () => {
   return (
     <Layout>
       <div className={styles.details}>
         <h2>title</h2>
         <h3>stack</h3>
-        <div className={styles.featured}>{/* <Img fluid={} /> */}</div>
+        <div className={styles.featured}>{/* <img fluid={}/> */}</div>
         {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
       </div>
     </Layout>
   )
 }
+
+export default ProjectDetails
