91b2bb8fe504aad29aef579dd1e6601d34c20c1b
Abraham Choi <abraham.choi@icloud.com>
2021-04-23 21:10:47 -0700

    Featured Images

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/projects.module.css"
+import Img from "gatsby-image"
 
 export default function Projects({ data }) {
   const projects = data.projects.nodes
@@ -15,8 +16,11 @@ export default function Projects({ data }) {
         <div className={styles.projects}>
           {projects.map(project => (
             <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
-              <h3>{project.frontmatter.title}</h3>
-              <p>{project.frontmatter.stack}</p>
+              <div>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
             </Link>
           ))}
         </div>
@@ -36,6 +40,20 @@ export const query = graphql`
           slug
           stack
           title
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          featuredImg {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
         }
         id
       }
